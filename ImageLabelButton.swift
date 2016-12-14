e1046a46ecf1343f134f94a0af0e6de69ba6836d
Patrick Gao <patrickgao0922@gmail.com>
2016-12-14 16:06:34 +1100

    AddAnimation

--- ImageLabelButton.swift
+++ ImageLabelButton.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+@IBDesignable
 class ImageLabelButton: UIButton{
     /*
      // Only override draw() if you perform custom drawing.
@@ -21,6 +22,17 @@ class ImageLabelButton: UIButton{
     //    let stackView = UIStackView()
     
     
+    
+    // Override property observors
+    @IBInspectable
+    var textColor:UIColor? {
+        get {
+            return buttonLabel.textColor
+        }
+        set(newValue) {
+            self.buttonLabel.textColor = newValue
+        }
+    }
     
     @IBInspectable
     var labelText:String? {
@@ -66,9 +78,9 @@ class ImageLabelButton: UIButton{
     var imageLabelDistance: CGFloat = 8.0
     
     convenience init() {
-        print("ttttt")
         self.init(frame:CGRect.zero)
-        self.setUpButton()
+        // Init button
+        self.initButton()
     }
     
     override init(frame:CGRect){
@@ -77,31 +89,38 @@ class ImageLabelButton: UIButton{
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
-        self.setUpButton()
+        self.initButton()
     }
     
     // Init for loading in Interface Builder
     override func prepareForInterfaceBuilder() {
-        self.setUpButton()
+        self.initButton()
     }
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.setUpButton()
+        self.initButton()
     }
     override func layoutSubviews() {
-        self.setUpButton()
+        self.initButton()
     }
     
-    func setUpButton(){
+    func initButton(){
+        // Init computed properties
+        self.imageLabelDistance = 8.0
+        self.imageWidth = 35.0
+        self.imageHeight = 35.0
+        self.labelText = "Button Label"
+        self.textColor = UIColor(red: 216.0/255.0, green: 216.0/255.0, blue: 216.0/255.0, alpha: 1.0)
         
-        self.frame.size.height = 62
+        
         buttonImgView.frame.size = CGSize(width:imageWidth, height:imageHeight)
-        buttonImgView.frame.origin = CGPoint(x: self.frame.width / 2.0 - buttonImgView.frame.width / 2.0, y: 15.5)
+        buttonImgView.frame.origin = CGPoint(x: self.frame.width / 2.0 - buttonImgView.frame.width / 2.0, y: 8.0)
         buttonImgView.contentMode = .center
         
         buttonLabel.frame.size = CGSize(width: 45.5, height:11.0)
         buttonLabel.frame.origin = CGPoint(x: self.frame.width / 2.0 - buttonLabel.frame.width / 2.0, y: (imageView?.frame.origin.y)! + imageHeight+8.0)
         buttonLabel.font = UIFont.systemFont(ofSize: 11.0)
+        self.frame.size.height = 8.0 + self.imageHeight + self.imageLabelDistance + self.buttonLabel.frame.size.height + 8.0
         
         //        stackView.addArrangedSubview(buttonImgView)
         //        stackView.addArrangedSubview(buttonLabel)
@@ -116,5 +135,23 @@ class ImageLabelButton: UIButton{
         self.addSubview(buttonLabel)
         //        stackView.centerXAnchor.constraintEqualToAnchor((superview?.centerXAnchor)!).active = true
         //        stackView.centerYAnchor.constraintEqualToAnchor((superview?.centerYAnchor)!).active = true
+    }
+    
+    func scaleOut() {
+        UIView.animate(withDuration: 0.3, animations: { 
+            self.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        }, completion: nil)
+    }
+    
+    func scaleIn() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.scaleOut()
+        self.scaleIn()
     }
 }
